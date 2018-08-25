d7f87260ecfe6b3a1f15bb1a4ddfa435e29b8ad1
Tere Valdayo <tere_valdayo@hotmail.com>
2018-08-26 01:04:44 +0200

    version mobile, terminada :ok_woman:

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 
+$(".mainNav li").on("click" , iraSeccion ) ;
+
+// var altoHeader = $(".pageHeader").height();
+var altoLogo = $(".logo").height();
+
+
+function iraSeccion() {
+	
+	var $liPulsado = $(this) ;
+	
+	var numLiPulsado = $liPulsado.index() ;
+	
+	var $seccionDestino = $("main section").eq(numLiPulsado) ;
+	
+	var scrollTopDestino = $seccionDestino.offset().top - altoLogo ;
+		
+    $("body,html").animate({ scrollTop: scrollTopDestino } , 2000) ;
+    $('#mobileBtn-chk').prop('checked', false); 
+
+}
+
+
+$(".logo").on("click" , irArriba ) ;
+
+function irArriba() {
+	
+	$("body,html").animate({ scrollTop: 0 } , 2000) ;
+	
+}
 
 
 
