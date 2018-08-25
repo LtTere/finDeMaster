b0bb1adaaf93b30e8796c5a39edc11d14da9b261
Tere Valdayo <tere_valdayo@hotmail.com>
2018-08-26 01:31:58 +0200

    ajustada velocidad del scroll

--- js/main.js
+++ js/main.js
@@ -4,18 +4,36 @@ $(".mainNav li").on("click" , iraSeccion ) ;
 // var altoHeader = $(".pageHeader").height();
 var altoLogo = $(".logo").height();
 
+var origen = 0
 
 function iraSeccion() {
 	
 	var $liPulsado = $(this) ;
+	var numLiPulsado = $liPulsado.index();
+	console.log(numLiPulsado,"LI PULSADO");
 	
-	var numLiPulsado = $liPulsado.index() ;
 	
-	var $seccionDestino = $("main section").eq(numLiPulsado) ;
+	var $seccionDestino = $("main section").eq(numLiPulsado);
+	var $seccionOrigen = $("main section").eq(origen);
 	
 	var scrollTopDestino = $seccionDestino.offset().top - altoLogo ;
-		
-    $("body,html").animate({ scrollTop: scrollTopDestino } , 2000) ;
+	var scrollTopOrigen = $seccionOrigen.offset().top - altoLogo;
+
+	var resta = scrollTopDestino - scrollTopOrigen;
+
+	if(resta < 0){
+		resta = resta * -1
+	}
+
+	origen = numLiPulsado;
+	
+	var velocidad = 2000;
+
+	if (resta < 500){
+		velocidad = 1000
+	}
+	
+    $("body,html").animate({ scrollTop: scrollTopDestino } , velocidad) ;
     $('#mobileBtn-chk').prop('checked', false); 
 
 }
